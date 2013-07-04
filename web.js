61c554d7a3220c330e841a4547728c90f604c798
manOfCode <shreeda.5687@gmail.com>
2013-07-05 00:24:50 +0530

    new file:   index.html 	modified:   web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!!');
+    var index = fs.readFileSync('index.html');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
